d0eb72ea3cefc7a31188e9439155ee0cd3543cc1
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-04-29 16:01:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Feladat
+Milyen hibákat találsz az alábbi JavaScript kódban?
+```
+var a = 5
+var b = 10;
+
+function sum() {
+  c = a + b;
+  console.log('The sum is: ' + c);
+}
+
+sum;
+```
---
+++ app.js
@@ -0,0 +1,16 @@
+const number1 = 5; const number2 = 10;
+
+
+function sum(num1, num2) {
+    if ((typeof num1 == "number") && (typeof num2 == "number")) {
+        return number1 + number2;
+    } else
+        throw "Numbers required!"
+}
+
+try {
+    const result = sum(number1, number2);
+    console.log(`The sum is: ${result}`);
+} catch (err) {
+    console.log(err);
+}
